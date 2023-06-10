2bea7aea2f27b73aacf6e21532be3a5f658b45a6
LinQuid <LinQuid@STATION>
2023-06-10 06:48:56 +0300

    C:/Program Files/Git/

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FileManipLib;
+
+namespace LibExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> list = new List<string>();
+            string a = "any text for my file";
+            list.Add(a);
+            FileManipulations.mySer(list, "Text.json");
+        }
+    }
+}
